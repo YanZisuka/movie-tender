398d3e224a46b36d4beee9180290042fa530e76c
YanZisuka <hanndrednine@gmail.com>
2022-05-21 01:18:35 +0900

    [FIX] Comment created_at, updated_at 추가

--- server/community/models.py
+++ server/community/models.py
@@ -25,6 +25,8 @@ class Comment(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
 
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Comment {self.pk}: {self.user}'s comment for {self.review}"

835ff1bbe97565a9e69019f0a79615d4c2c44401
Yeonggyeong <jeljelkim129@gmail.com>
2022-05-24 14:13:40 +0900

    add div tag

--- client/src/views/SignupView.vue
+++ client/src/views/SignupView.vue
@@ -1,5 +1,27 @@
 <template>
-  
+  <div>
+    <h1>Signup</h1>
+
+    <account-error-list v-if="authError"></account-error-list>
+
+    <form @submit.prevent="signup(credentials)">
+      <div>
+        <label for="username">Username: </label>
+        <input  v-model="credentials.username" type="text" id="username" required/>
+      </div>
+      <div>
+        <label for="password1">Password: </label>
+        <input v-model="credentials.password1" type="password" id="password1" required />
+      </div>
+      <div>
+        <label for="password2">Password Confirmation:</label>
+        <input v-model="credentials.password2" type="password" id="password2" required />
+      </div>
+      <div>
+        <button>Signup</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>

c19e568b1c5144d051d844a2e1c0262b9566f863
YanZisuka <hanndrednine@gmail.com>
2022-06-19 01:06:47 +0900

    [ADD] movies.views test code

---
+++ server/accounts/tests/factories.py
@@ -0,0 +1,26 @@
+import random
+
+import faker
+
+from django.contrib.auth import get_user_model
+
+from factory import Faker, LazyFunction
+from factory.django import DjangoModelFactory
+
+
+def _random_array_fields():
+    _faker = faker.Faker()
+    return _faker.words(nb=random.randrange(1, 10), unique=True)
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+    
+    username = Faker('first_name')
+    nickname = Faker('first_name')
+    first_name = faker.Faker('ko-KR').first_name()
+    last_name = faker.Faker('ko-KR').last_name()
+    email = Faker('ascii_email')
+    survey = LazyFunction(_random_array_fields)
+    
--- server/movies/tests/test_views.py
+++ server/movies/tests/test_views.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        res = Client().post('/api/v1/accounts/signup/', \
+            data={'username': 'credential', 'nickname': 'RandomNick', 'password1': 'qwer`123', 'password2': 'qwer`123'})
+        token = res.json().get('key')
+        cls.header = {'HTTP_AUTHORIZATION': f'Token {token}'}
+
+    def test_영화추천을_받을수있다(self):
+        res = self.client.get(reverse('movies:index'), **self.header)
+
+        self.assertIs(res.status_code, 200)
+
+
+class MovieTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        res = Client().post('/api/v1/accounts/signup/', \
+            data={'username': 'credential', 'nickname': 'RandomNick', 'password1': 'qwer`123', 'password2': 'qwer`123'})
+        token = res.json().get('key')
+        cls.header = {'HTTP_AUTHORIZATION': f'Token {token}'}
+
+    def test_영화상세정보를_볼수있다(self):
+        res = self.client.get(reverse('movies:movie', args=[7]), **self.header)
+
+        self.assertIs(res.status_code, 200)

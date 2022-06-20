2cdf7f94ab350846eaa4619971e1d455fbc2981d
YanZisuka <hanndrednine@gmail.com>
2022-06-20 18:25:10 +0900

    [ADD] Test get_reviews

---
+++ server/community/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class ReviewsViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        res = Client().post('/api/v1/accounts/signup/', \
+            data={'username': 'credential', 'nickname': 'RandomNick', 'password1': 'qwer`123', 'password2': 'qwer`123'})
+        token = res.json().get('key')
+        cls.header = {'HTTP_AUTHORIZATION': f'Token {token}'}
+
+
+    def test_리뷰들을_받아올수있다(self):
+        res = self.client.get(reverse('community:index'), **self.header)
+
+        self.assertEqual(res.status_code, 200)
+        

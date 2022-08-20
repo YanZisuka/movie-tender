3f9b9582d6f2585d7f78ef06f165881492c34409
YanZisuka <hanndrednine@gmail.com>
2022-08-21 01:21:00 +0900

    :white_check_mark: [TEST] black formatter 적용

--- server/movies/tests/test_models.py
+++ server/movies/tests/test_models.py
@@ -4,52 +4,51 @@ from movies.models import Movie
 from .factories import MovieFactory
 
 
-class MovieTest(TestCase):
+class MovieModelTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.movie = MovieFactory.create(_genres=['a', 'b'], _keywords=['a', 'b'], _providers=['a', 'b'])
-
+        cls.movie = MovieFactory.create(
+            _genres=["a", "b"], _keywords=["a", "b"], _providers=["a", "b"]
+        )
 
     def test_장르가_있는지_검사할수있다(self):
-        assert self.movie.genres.contains('a')
-        assert not self.movie.genres.contains('nono')
+        assert self.movie.genres.contains("a")
+        assert not self.movie.genres.contains("nono")
 
     def test_장르를_추가할수있다(self):
-        self.movie.genres.add('word')
+        self.movie.genres.add("word")
 
-        assert self.movie.genres.set == {'a', 'b', 'word'}
+        assert self.movie.genres.set == {"a", "b", "word"}
 
     def test_장르를_삭제할수있다(self):
-        self.movie.genres.remove('a')
+        self.movie.genres.remove("a")
 
-        assert self.movie.genres.set == {'b'}
-
+        assert self.movie.genres.set == {"b"}
 
     def test_키워드가_있는지_검사할수있다(self):
-        assert self.movie.keywords.contains('a')
-        assert not self.movie.keywords.contains('nono')
+        assert self.movie.keywords.contains("a")
+        assert not self.movie.keywords.contains("nono")
 
     def test_키워드를_추가할수있다(self):
-        self.movie.keywords.add('word')
+        self.movie.keywords.add("word")
 
-        assert self.movie.keywords.set == {'a', 'b', 'word'}
+        assert self.movie.keywords.set == {"a", "b", "word"}
 
     def test_키워드를_삭제할수있다(self):
-        self.movie.keywords.remove('a')
+        self.movie.keywords.remove("a")
 
-        assert self.movie.keywords.set == {'b'}
-
+        assert self.movie.keywords.set == {"b"}
 
     def test_프로바이더가_있는지_검사할수있다(self):
-        assert self.movie.providers.contains('a')
-        assert not self.movie.providers.contains('nono')
+        assert self.movie.providers.contains("a")
+        assert not self.movie.providers.contains("nono")
 
     def test_프로바이더를_추가할수있다(self):
-        self.movie.providers.add('word')
+        self.movie.providers.add("word")
 
-        assert self.movie.providers.set == {'a', 'b', 'word'}
+        assert self.movie.providers.set == {"a", "b", "word"}
 
     def test_프로바이더를_삭제할수있다(self):
-        self.movie.providers.remove('a')
+        self.movie.providers.remove("a")
 
-        assert self.movie.providers.set == {'b'}
+        assert self.movie.providers.set == {"b"}
--- server/movies/tests/test_views.py
+++ server/movies/tests/test_views.py
@@ -11,84 +11,112 @@ from accounts.tests.factories import UserFactory
 from ..models import Movie
 
 
-class MoviesViewsTest(TestCase):
+class MovieViewSetTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         credentials = UserFactory.build()
-        credentials.password = 'qwer`123'
-        
-        res = Client().post('/api/v1/accounts/signup/', 
+        credentials.password = "qwer`123"
+
+        res = Client().post(
+            "/api/v1/accounts/signup/",
             data={
-                'username': credentials.username, 
-                'nickname': credentials.nickname, 
-                'password1': credentials.password, 
-                'password2': credentials.password,
-            })
-        token = res.json().get('key')
-        cls.header = {'HTTP_AUTHORIZATION': f'Token {token}'}
+                "username": credentials.username,
+                "nickname": credentials.nickname,
+                "password1": credentials.password,
+                "password2": credentials.password,
+            },
+        )
+        token = res.json().get("key")
+        cls.header = {"HTTP_AUTHORIZATION": f"Token {token}"}
 
         cls.user = get_user_model().objects.get(username=credentials.username)
         cls.movies = MovieFactory.build_batch(100)
         Movie.objects.bulk_create(cls.movies)
         cls.movie = MovieFactory.create(vote_average=0, vote_count=0)
-        cls.keywords = [KeywordFactory.create(keyword='anime'), KeywordFactory.create(keyword='superhero')]
-
+        cls.keywords = [
+            KeywordFactory.create(keyword="anime"),
+            KeywordFactory.create(keyword="superhero"),
+        ]
 
     def test_영화추천을_받을수있다(self):
         # survey 없는 경우
-        res = self.client.get(reverse('movies:index'), **self.header)
+        res = self.client.get(reverse("movies:movies"), **self.header)
         self.assertEqual(res.status_code, 400)
-        
-        # survey 있는 경우
-        self.user.survey = ['anime', 'superhero'] + [a for a in ascii_lowercase]
-        self.user.save(update_fields=['survey'])
 
-        res = self.client.get(reverse('movies:index'), **self.header)
+        # survey 있는 경우
+        self.user.survey = ["anime", "superhero"] + [a for a in ascii_lowercase]
+        self.user.save(update_fields=["survey"])
+
+        res = self.client.get(reverse("movies:index"), **self.header)
         self.assertEqual(res.status_code, 200)
 
     def test_서베이를_설정할수있다(self):
-        data = json.dumps({
-            'survey': ['anime', 'superhero']
-        })
-        res = self.client.put(reverse('movies:index'), data=data, content_type='application/json', **self.header)
+        data = json.dumps({"survey": ["anime", "superhero"]})
+        res = self.client.put(
+            reverse("movies:movies"),
+            data=data,
+            content_type="application/json",
+            **self.header,
+        )
 
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(get_user_model().objects.get(pk=self.user.id).survey, ['anime', 'superhero'])
-
+        self.assertEqual(
+            get_user_model().objects.get(pk=self.user.id).survey, ["anime", "superhero"]
+        )
 
     def test_영화상세정보를_볼수있다(self):
-        res = self.client.get(reverse('movies:movie', args=[self.movies[0].id]), **self.header)
+        res = self.client.get(
+            reverse("movies:movie", args=[self.movies[0].id]), **self.header
+        )
 
         self.assertEqual(res.status_code, 200)
 
     def test_평점을_설정할수있다(self):
-        res = self.client.post(reverse('movies:movie', args=[self.movie.id]), data={'rating': 3.5}, **self.header)
+        res = self.client.post(
+            reverse("movies:movie", args=[self.movie.id]),
+            data={"rating": 3.5},
+            **self.header,
+        )
         self.assertEqual(res.status_code, 201)
         self.assertEqual(Movie.objects.get(pk=self.movie.id).vote_average, 3.5)
         self.assertEqual(Movie.objects.get(pk=self.movie.id).vote_count, 1)
 
-        res = self.client.post(reverse('movies:movie', args=[self.movie.id]), data={'rating': 5}, **self.header)
+        res = self.client.post(
+            reverse("movies:movie", args=[self.movie.id]),
+            data={"rating": 5},
+            **self.header,
+        )
         self.assertEqual(res.status_code, 201)
         self.assertEqual(Movie.objects.get(pk=self.movie.id).vote_average, 5)
         self.assertEqual(Movie.objects.get(pk=self.movie.id).vote_count, 1)
 
-
     def test_평점을_불러올수있다(self):
-        res = self.client.get(reverse('movies:get_rating', args=[self.movies[0].id, self.user.username]), **self.header)
+        res = self.client.get(
+            reverse("movies:get_rating", args=[self.movies[0].id, self.user.username]),
+            **self.header,
+        )
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.json().get('rating'), 0)
+        self.assertEqual(res.json().get("rating"), 0)
 
-        res = self.client.post(reverse('movies:movie', args=[self.movies[0].id]), data={'rating': 4}, **self.header)
-        res = self.client.get(reverse('movies:get_rating', args=[self.movies[0].id, self.user.username]), **self.header)
+        res = self.client.post(
+            reverse("movies:movie", args=[self.movies[0].id]),
+            data={"rating": 4},
+            **self.header,
+        )
+        res = self.client.get(
+            reverse("movies:get_rating", args=[self.movies[0].id, self.user.username]),
+            **self.header,
+        )
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.json().get('rating'), 4)
+        self.assertEqual(res.json().get("rating"), 4)
 
-    
     def test_키워드에_맞는_영화를_뽑을수있다(self):
-        self.movies[0].keywords.add('anime')
-        self.movies[1].keywords.add('superhero')
+        self.movies[0].keywords.add("anime")
+        self.movies[1].keywords.add("superhero")
 
-        res = self.client.get(reverse('movies:get_movies_with_keywords', args=[2]), **self.header)
+        res = self.client.get(
+            reverse("movies:get_movies_with_keywords", args=[2]), **self.header
+        )
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(len(res.data), 2)

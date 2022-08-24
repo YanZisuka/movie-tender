3e15c6b6c8ce1069d879e71d49fd21f51837cde1
YanZisuka <hanndrednine@gmail.com>
2022-08-25 03:02:45 +0900

    :recycle: [ADD] collectstatic 설정 추가

--- server/server/settings.py
+++ server/server/settings.py
@@ -198,6 +198,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, ".static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

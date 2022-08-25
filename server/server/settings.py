b4d9d52d964505f9f48a196e6c3154db3b3248fd
YanZisuka <hanndrednine@gmail.com>
2022-08-25 16:03:51 +0900

    :ambulance: [FIX] CSRF_TRUSTED_ORIGINS 추가

--- server/server/settings.py
+++ server/server/settings.py
@@ -224,9 +224,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "https://movietender.link",
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://movietender.link",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [

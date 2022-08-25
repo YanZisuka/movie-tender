585e85777059be51d92a70dcd8a34a58fe69a70d
YanZisuka <hanndrednine@gmail.com>
2022-08-25 21:08:36 +0900

    :lipstick: [DESIGN] Auth 관련 UI 수정

--- client/src/components/LoginModal.vue
+++ client/src/components/LoginModal.vue
@@ -1,89 +1,91 @@
 <template>
   <div>
-
     <div class="login-body d-flex flex-column text-start pt-3 pb-5 px-5">
-    <account-error-list v-if="authError"></account-error-list>
-
       <h1 class="">로그인</h1>
 
-      <form @submit.prevent="login(credentials); switchShowAccountModal();" class="d-flex flex-column">
+      <form @submit.prevent="login(credentials)" class="d-flex flex-column">
         <div class="mt-4 mb-3">
           <div>
             <label for="username" class="">아이디</label>
           </div>
-          <input v-model="credentials.username" type="text" id="username" placeholder="ID" required>
+          <input
+            v-model="credentials.username"
+            type="text"
+            id="username"
+            placeholder="ID"
+            required
+          />
         </div>
 
         <div class="mt-3">
           <div>
             <label for="password" class="">비밀번호</label>
           </div>
-          <input v-model="credentials.password" type="password" id="password" placeholder="Password" required>
+          <input
+            v-model="credentials.password"
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
         </div>
 
         <div class="mt-1 mb-2">
-          <span @click="$emit('signup-emit')" class="">아직 회원이 아니신가요? 회원가입</span>
+          <span @click="$emit('signup-emit')" class=""
+            >아직 회원이 아니신가요? 회원가입</span
+          >
         </div>
 
-        <button class="btn-theme align-self-center mt-5">로그인</button>
+        <button class="button align-self-center mt-5">로그인</button>
       </form>
-
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-
-import AccountErrorList from '@/components/AccountErrorList.vue'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: 'LoginModal',
-
-  components: {
-    AccountErrorList,
-  },
+  name: "LoginModal",
 
   data() {
     return {
       credentials: {
-        username: '',
-        password: '',
+        username: "",
+        password: "",
       },
-    }
+    };
   },
 
   computed: {
-    ...mapGetters(['authError']),
+    ...mapGetters(["authError"]),
   },
 
   methods: {
-    ...mapActions(['login', 'switchShowAccountModal',])
+    ...mapActions(["login", "switchShowAccountModal"]),
   },
-
-}
+};
 </script>
 
-<style scoped>
-.login-body h1 {
-  font-weight: 700;
-}
-
-.login-body label {
-  font-weight: 700;
-  margin: 0 0 3px 0;
-}
-
-.login-body input {
-  width: 100%;
-  height: 2.5rem;
-  border: 2px solid var(--gray);
-  border-radius: 8px;
-  padding: 0 1rem;
-}
-
-.login-body span:hover {
-  cursor: pointer;
-  text-decoration-line: underline;
+<style scoped lang="scss">
+.login-body {
+  h1 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+  label {
+    font-weight: normal;
+    margin: 0 0 3px 0;
+  }
+  input {
+    height: 2.5rem;
+    padding: 0 1rem;
+  }
+  span {
+    &:hover {
+      cursor: pointer;
+      text-decoration-line: underline;
+    }
+  }
 }
 </style>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,58 +1,79 @@
 <template>
-  <nav class="d-flex justify-content-between sticky-top p-3">
-    <router-link :to="{ name: 'index' }" class="navbar-brand mx-5">
-      <div :class="isDark ? 'text-fff' : 'text-theme'" class="navbar-logo">movietender</div>
-    </router-link>
-    <div class="d-flex align-items-center">
+  <nav class="row d-flex sticky-top">
+    <auth-error v-if="authError"></auth-error>
 
+    <div class="d-flex col-6">
+      <router-link :to="{ name: 'index' }" class="navbar-brand">
+        <h1 :class="isDark ? 'text-fff' : 'text-theme'" class="navbar-logo">
+          movietender
+        </h1>
+      </router-link>
       <router-link
         :to="{ name: 'movies' }"
         :class="isDark ? 'text-fff' : 'text-theme'"
-        class="text-decoration-none mx-5"
+        class="text-decoration-none pt-2 ms-5"
         v-if="isLoggedIn"
-        >OMAKASE</router-link>
-
-      <router-link
+        >영화추천
+      </router-link>
+    </div>
+    <div class="d-flex justify-content-end align-items-center col-6">
+      <!-- <router-link
         :to="{ name: 'reviews' }"
         :class="isDark ? 'text-fff' : 'text-theme'"
-        class="text-decoration-none mx-5"
-        >COMMUNITY</router-link>
+        class="text-decoration-none"
+        >COMMUNITY</router-link
+      > -->
 
-      <div v-if="isLoggedIn" class="dropdown mx-5">
-        <button
-          :class="isDark ? 'text-fff' : 'text-theme'"
-          class="btn dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-          ><img class="profile-img me-2" :src="profileImg" alt="profile-img">
-        </button>
+      <div
+        v-if="isLoggedIn"
+        class="profile"
+        @mouseenter="setIsDisplayProfileMenu(true)"
+        @click="setIsDisplayProfileMenu(true)"
+        @mouseleave="setIsDisplayProfileMenu(false)"
+      >
+        <img class="profile-img me-3" :src="profileImg" alt="profile-img" />
+        <i class="fa-solid fa-chevron-down"></i>
 
-        <div class="profile dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <router-link
-            :to="{ name: 'profile', params: { username: currentUser.username } }"
-            class="text-decoration-none"
-            role="button">
-            <p class="text-fff text-center m-0">
-            Profile
-            </p>
-          </router-link>
-          <hr class="text-white m-1">
+        <div v-if="isDisplayProfileMenu" class="menu">
+          <div class="d-flex w-100">
+            <img :src="profileImg" alt="" />
+            <div class="d-flex flex-column justify-content-center ms-2">
+              <h1>{{ profile.nickname }}</h1>
+              <h2>{{ profile.username }}</h2>
+            </div>
+          </div>
+          <hr />
           <router-link
             :to="{ name: 'logout', params: { username: currentUser.username } }"
             class="text-decoration-none"
-            role="button">
-            <p class="text-fff text-center m-0">
-            Logout
-            </p>
-          </router-link> 
+          >
+            <button>로그아웃</button>
+          </router-link>
         </div>
       </div>
 
-      <button v-else @click="switchShowAccountModal()" class="navbar-btn mx-5">
-        Login
+      <!-- <router-link
+          :to="{
+            name: 'profile',
+            params: { username: currentUser.username },
+          }"
+          class="text-decoration-none"
+          role="button"
+        >
+          <p class="text-fff text-center m-0">Profile</p>
+        </router-link>
+        <hr class="text-white m-1" />
+        <router-link
+          :to="{ name: 'logout', params: { username: currentUser.username } }"
+          class="text-decoration-none"
+          role="button"
+        >
+          <p class="text-fff text-center m-0">Logout</p>
+        </router-link>
+      </div> -->
+
+      <button v-else @click="switchShowAccountModal()" class="navbar-btn">
+        로그인/회원가입
       </button>
     </div>
 
@@ -60,34 +81,34 @@
     <modal-detail :show="showAccountModal">
       <div slot="body" class="modal-body d-flex flex-column">
         <button
-          class="btn align-self-end mt-2 me-2"
+          class="align-self-end mt-2 me-2"
           @click="switchShowAccountModal()"
-          ><i class="fa-solid fa-xmark"></i></button>
+        >
+          <i class="fa-solid fa-xmark"></i>
+        </button>
         <login-modal
           v-if="!isSignUp"
           @signup-emit="isSignUp = true"
-          ></login-modal>
-        <signup-modal
-          v-else
-          @login-emit="isSignUp = false"
-          ></signup-modal>
+        ></login-modal>
+        <signup-modal v-else @login-emit="isSignUp = false"></signup-modal>
       </div>
     </modal-detail>
-
   </nav>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
-import ModalDetail from '@/components/ModalDetail.vue'
-import LoginModal from '@/components/LoginModal.vue'
-import SignupModal from '@/components/SignupModal.vue'
+import AuthError from "@/components/AuthError.vue";
+import ModalDetail from "@/components/ModalDetail.vue";
+import LoginModal from "@/components/LoginModal.vue";
+import SignupModal from "@/components/SignupModal.vue";
 
 export default {
-  name: 'NavBar',
+  name: "NavBar",
 
   components: {
+    AuthError,
     ModalDetail,
     LoginModal,
     SignupModal,
@@ -99,73 +120,151 @@ export default {
 
   data() {
     return {
-      profileImg: require('@/assets/default-profile.png'),
+      profileImg: require("@/assets/default-profile.png"),
       isSignUp: false,
-    }
+      isDisplayProfileMenu: false,
+    };
   },
 
   computed: {
-    ...mapGetters(['isLoggedIn', 'currentUser', 'authError', 'showAccountModal',]),
+    ...mapGetters([
+      "isLoggedIn",
+      "currentUser",
+      "profile",
+      "authError",
+      "showAccountModal",
+    ]),
   },
 
   methods: {
-    ...mapActions(['switchShowAccountModal',]),
+    ...mapActions([
+      "fetchCurrentUser",
+      "fetchProfile",
+      "switchShowAccountModal",
+    ]),
+    setIsDisplayProfileMenu(payload) {
+      this.isDisplayProfileMenu = payload;
+    },
   },
-}
+
+  async created() {
+    await this.fetchCurrentUser();
+    this.fetchProfile(this.currentUser);
+  },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 nav {
-  padding: 2rem;
-}
+  height: 85px;
+  padding: 0 2rem;
 
-nav a {
-  font-weight: 700;
-  color: var(--black);
-}
+  @include lg {
+    padding: 0 5rem;
+  }
 
-nav a.router-link-exact-active {
-  color: var(--mainBgColor);
-}
+  a {
+    font-weight: bold;
+    color: $blackColor;
+    display: flex;
+    align-items: center;
+    font-size: 0.9rem;
+  }
 
+  a.router-link-exact-active {
+    color: $mainBadgeColor;
+  }
+}
 .navbar-logo {
-  font-family: 'Lobster', cursive;
+  font-family: "Lobster", cursive;
   font-size: 2rem;
 }
-
 .navbar-btn {
-  background-color: var(--mainBgColor);
-  width: 5rem;
-  height: 28px;
+  background-color: transparent;
+  height: 2rem;
   border-radius: 16px;
   border: 0;
-  color: var(--white);
-}
+  font-weight: bold;
+  color: $whiteColor;
 
+  &:hover {
+    color: $adaptiveGrey200;
+  }
+}
 .profile {
-  background-color: rgba(0, 0, 0, 0.5);
-  border: 0;
-  margin: 0 75px 0 0;
-}
+  margin-left: 5rem;
+  color: $adaptiveGrey100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 
+  &:hover {
+    cursor: pointer;
+
+    i {
+      transform: rotate(180deg);
+      transition: transform 0.15s ease;
+    }
+  }
+}
 .profile-img {
   width: 2rem;
+  border-radius: 4px;
 }
-
-.text-fff {
-  color: var(--white);
-}
-
 .text-theme {
-  color: var(--black);
+  color: $blackColor;
 }
-
 .modal-body {
   display: inline-block;
-  background: var(--white);
+  background: $whiteColor;
   /* border: 2px solid var(--black); */
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   border-radius: 16px;
-}
 
+  button {
+    background-color: transparent;
+    border: 0;
+    color: $adaptiveGrey800;
+
+    &:hover {
+      color: $adaptiveGrey500;
+    }
+  }
+}
+.menu {
+  width: 10rem;
+  background-color: $offwhiteColor;
+  position: fixed;
+  top: 60px;
+  right: 5rem;
+  border-radius: 4px;
+  color: $blackColor;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.3rem 0.1rem;
+  filter: drop-shadow(0 0 1rem $blackColor);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 1rem 0.5rem 0.5rem 0.5rem;
+
+  img {
+    width: 2.5rem;
+    border-radius: 4px;
+  }
+  h1 {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+  hr {
+    width: 100%;
+    margin: 0.5rem;
+  }
+  button {
+    border: 0;
+    background-color: transparent;
+  }
+}
 </style>

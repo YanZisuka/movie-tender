38fc21a821e7d997eda97e0371e2127e393e9f93
YanZisuka <hanndrednine@gmail.com>
2022-08-25 21:09:34 +0900

    :lipstick: [DESIGN] Index UI 수정

--- client/src/views/IndexView.vue
+++ client/src/views/IndexView.vue
@@ -1,55 +1,66 @@
 <template>
-  <div class="index d-flex flex-column justify-content-center align-items-center">
+  <div class="index">
     <video muted autoplay loop>
-      <source :src="bgVideo" type="video/mp4">
+      <source :src="bgVideo" type="video/mp4" />
     </video>
 
-    <h1 class="logo">movietender</h1>
-    <p class="index-text">지금,</p>
-    <p class="index-text mb-4">어떤 영화를 볼지 고민된다면?</p>
-    <!-- <button v-if="!isLoggedIn" @click="switchShowAccountModal()" class="btn-theme-lg">Login!</button> -->
-    <!-- <router-link v-else :to="{ name: 'logout', params: { username: currentUser.username } }">
-      <button class="btn-theme-lg">Logout!</button>
-    </router-link> -->
-
+    <div class="d-flex flex-column justify-content-center text-center">
+      <h1>movietender</h1>
+      <p>지금,</p>
+      <p class="mb-4">어떤 영화를 볼지 고민된다면?</p>
+      <button
+        v-if="!isLoggedIn"
+        @click="switchShowAccountModal()"
+        class="button"
+      >
+        로그인
+      </button>
+      <router-link
+        v-else
+        :to="{ name: 'logout', params: { username: currentUser.username } }"
+      >
+        <button class="button">로그아웃</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  name: 'IndexView',
+  name: "IndexView",
 
   data() {
     return {
-      bgVideo: require('@/assets/bg-video.mp4'),
-    }
+      bgVideo: require("@/assets/bg-video.mp4"),
+    };
   },
 
   computed: {
-    ...mapGetters(['isLoggedIn', 'currentUser']),
+    ...mapGetters(["isLoggedIn", "currentUser"]),
   },
 
   methods: {
-    ...mapActions(['fetchCurrentUser', 'switchShowAccountModal'])
+    ...mapActions(["switchShowAccountModal"]),
   },
 
   created() {
-    this.$emit('dark-emit')
-    document.body.setAttribute('style', 'background-color: #fafafa;')
-    this.fetchCurrentUser()
+    document.body.style.backgroundColor = "#171717";
+    this.$emit("dark-emit");
   },
-}
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .index {
-  width: 100%;
-  height: 80vh;
-}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 85px);
 
-  .index > video {
+  video {
     width: 100vw;
     height: 100vh;
     object-fit: cover;
@@ -61,21 +72,18 @@ export default {
     z-index: -1;
     filter: brightness(40%);
   }
-
-.logo {
-  font-family: 'Lobster', cursive;
-  font-size: 4rem;
-  font-weight: 700;
-  color: #fff;
-}
-
-.index-text {
-  font-size: 2rem;
-  font-weight: 700;
-  line-height: 1.5;
-  letter-spacing: -0.125rem;
-  margin: 0;
-  color: var(--black);
-  color: #fff;
+  h1 {
+    font-family: "Lobster", cursive;
+    font-size: 4rem;
+    font-weight: 700;
+    color: $whiteColor;
+  }
+  p {
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 1.5;
+    margin: 0;
+    color: $whiteColor;
+  }
 }
 </style>

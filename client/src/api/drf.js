d20586944d75e9261bfdfcb2b6c5e898e7febbb9
YanZisuka <hanndrednine@gmail.com>
2022-08-25 21:07:46 +0900

    :art: [STYLE] Prettier 적용

--- client/src/api/drf.js
+++ client/src/api/drf.js
@@ -1,31 +1,32 @@
-const HOST = 'http://localhost:8000/api/v1/'
+const HOST = "https://" + "movietender.link" + "/api/v1/";
 
-const ACCOUNTS = 'accounts/'
-const MOVIES = 'movies/'
-const COMMUNITY = 'reviews/'
-const COMMENTS = 'comments/'
+const ACCOUNTS = "accounts/";
+const MOVIES = "movies/";
+const COMMUNITY = "reviews/";
+const COMMENTS = "comments/";
 
 export default {
   accounts: {
-    login: () => HOST + ACCOUNTS + 'login/',
-    logout: () => HOST + ACCOUNTS + 'logout/',
-    signup: () => HOST + ACCOUNTS + 'signup/',
-    currentUserInfo: () => HOST + ACCOUNTS + 'user/',
-    profile: username => HOST + ACCOUNTS + `profile/${username}/`,
+    login: () => HOST + ACCOUNTS + "login/",
+    logout: () => HOST + ACCOUNTS + "logout/",
+    signup: () => HOST + ACCOUNTS + "signup/",
+    currentUserInfo: () => HOST + ACCOUNTS + "user/",
+    profile: (username) => HOST + ACCOUNTS + `profile/${username}/`,
   },
   movies: {
     movies: () => HOST + MOVIES,
-    movie: moviePk => HOST + MOVIES + `${moviePk}/`,
-    rating: (moviePk, username) => HOST + MOVIES + `${moviePk}/` + ACCOUNTS + `${username}/`,
-    movieStaff: () => HOST + MOVIES + 'staffs/',
-    movieGenres: genreGroup => HOST + MOVIES + `genres/${genreGroup}/`,
-    moviesWithKeyword: pickNum => HOST + MOVIES + `keywords/${pickNum}/`
+    movie: (moviePk) => HOST + MOVIES + `${moviePk}/`,
+    rating: (moviePk, username) =>
+      HOST + MOVIES + `${moviePk}/` + ACCOUNTS + `${username}/`,
+    movieStaff: () => HOST + MOVIES + "staffs/",
+    movieGenres: (genreGroup) => HOST + MOVIES + `genres/${genreGroup}/`,
+    moviesWithKeyword: (pickNum) => HOST + MOVIES + `keywords/${pickNum}/`,
   },
   community: {
     reviews: () => HOST + COMMUNITY,
-    review: reviewPk => HOST + COMMUNITY + `${reviewPk}/`,
-    createComment: reviewPk => HOST + COMMUNITY + `${reviewPk}/` + COMMENTS,
+    review: (reviewPk) => HOST + COMMUNITY + `${reviewPk}/`,
+    createComment: (reviewPk) => HOST + COMMUNITY + `${reviewPk}/` + COMMENTS,
     comment: (reviewPk, commentPk) =>
       HOST + COMMUNITY + `${reviewPk}/` + COMMENTS + `${commentPk}/`,
-  }
-}
+  },
+};

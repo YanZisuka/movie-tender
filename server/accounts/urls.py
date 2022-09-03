165153daedc8d9355811c48d366ae515d91227e2
YanZisuka <hanndrednine@gmail.com>
2022-09-03 23:32:16 +0900

    :art: [STYLE] UserViewSet 업데이트

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -2,8 +2,19 @@ from django.urls import path
 from . import views
 
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('profile/<str:username>/', views.UserView.as_view(), name='profile'),
+    path(
+        "profile/<str:username>/",
+        views.UserViewSet.as_view(
+            {
+                "get": "retrieve",
+                "post": "set_follow",
+                "put": "update",
+                "delete": "destroy",
+            }
+        ),
+        name="profile",
+    ),
 ]
